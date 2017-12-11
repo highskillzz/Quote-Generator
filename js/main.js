bfbf104eece9bc8c2e68b3167b2679cc2c77827e
saivicky2015 <saivicky2015@gmail.com>
2017-12-11 19:24:51 +0530

    final commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('#newquote').click(function(){
+      var color=['#F64747','#663399','#4183D7','#22313F','#9A12B3','#03A678']; 
+      var index=color[Math.floor(Math.random()*color.length)]; 
+      getRandomQuote();
+    });
+  });
+  
+
+  function getRandomQuote(){
+    var color=['#F64747','#663399','#4183D7','#22313F','#9A12B3','#03A678']; 
+    var index=color[Math.floor(Math.random()*color.length)]; 
+    $.ajax({
+        url: 'https://talaikis.com/api/quotes/random/',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var quote=data.quote;
+            var author=data.author;
+            $('.quote #data').html(quote);
+            $('.quote h4').html("-"+author);
+            $('body').css('background-color', index);
+            $('.col').css('background-color', index);
+            $('.socialmedia a').css('background-color', index);
+            $('#newquote').css('color','white');
+            $('.color').css('color', index);
+            $('#twitter').attr('href',"https://twitter.com/intent/tweet?text="+quote+" "+author);
+        },
+        error: function(err) {
+          $('.quote #data').html("Oops something went wrong.");
+        }
+      });
+  }
